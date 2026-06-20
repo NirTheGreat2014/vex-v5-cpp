79b8cf6889631088b7189288d62afc6b005fac0f
Nirbhay Ghimire <nirbhayghimire@gmail.com>
2026-06-20 12:22:53 -0400

    added variable c to total amount(d)

--- firstCppprogram.cpp
+++ firstCppprogram.cpp
@@ -4,8 +4,9 @@ using namespace std;
 int main() {
     int a = 20;
     int b = 30;
-    int c = a + b;
+    int c = 40;
+    int d = a + b + c;
     std::cout << "Hello World!" << endl;
-    std::cout << c << endl;
+    std::cout << d << endl;
     return 0;
 }
